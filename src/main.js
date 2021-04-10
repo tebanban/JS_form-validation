22b5ad34c5fcab7086e3501908e02229ac04cd29
tebanban <estebancalf@gmail.com>
2021-04-10 17:52:25 +0000

    prevent default

--- src/main.js
+++ src/main.js
@@ -9,4 +9,10 @@ window.onload = function() {
   console.log("Hello Rigo from the console!");
 };
 
-document.getElementById("sendButton");
+const sendButton = document.getElementById("google");
+const check = document.getElementById("check");
+
+sendButton.addEventListener("click", function(e) {
+  e.preventDefault();
+  console.log("event default ok");
+});
