7df1cee073ced4d9e3593b0fe0aefba36b3eaa8d
tebanban <estebancalf@gmail.com>
2021-04-10 22:39:36 +0000

    err messages!

--- src/main.js
+++ src/main.js
@@ -2,17 +2,29 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   console.log("Hello Rigo from the console!");
 };
 
-const sendButton = document.getElementById("google");
-const check = document.getElementById("check");
+const cardNumber = document.getElementById("cardNumber");
+const cvc = document.getElementById("cvc");
+const amount = document.getElementById("amount");
+
+const sendButton = document.getElementById("sendButton");
 
 sendButton.addEventListener("click", function(e) {
-  e.preventDefault();
-  console.log("event default ok");
+  let errMessages = [];
+  if (cardNumber.value === " " || cardNumber.value === null) {
+    errMessages.push("Name is equired");
+  }
+  if (cvc.value === " " || cvc.value === null) {
+    errMessages.push("Last name is required");
+  }
+  if (amount.value === " " || amount.value === null) {
+    errMessages.push("Last name is required");
+  }
+  if (errMessages.length > 0) {
+    e.preventDefault();
+    console.log("event default working");
+  }
 });
